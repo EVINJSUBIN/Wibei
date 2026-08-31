f3b94dfccf6b9d2dd14c28bd2ffcdcc0eaba4c9e
EVINJSUBIN <youridertech@gmail.com>
2026-08-31 19:26:28 +0530

    Add graceful SIGTERM/SIGINT shutdown handling to server.js

--- server.js
+++ server.js
@@ -160,6 +160,22 @@ app.get('/metadata', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
+});
+
+process.on('SIGTERM', () => {
+    console.log('SIGTERM signal received: closing HTTP server');
+    server.close(() => {
+        console.log('HTTP server closed cleanly');
+        process.exit(0);
+    });
+});
+
+process.on('SIGINT', () => {
+    console.log('SIGINT signal received: closing HTTP server');
+    server.close(() => {
+        console.log('HTTP server closed cleanly');
+        process.exit(0);
+    });
 });
